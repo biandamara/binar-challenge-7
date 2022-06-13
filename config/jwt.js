70508a4a2f060e18857e2ae08e0dc721a569a712
Bian Damara <bian.damara@gmail.com>
2022-06-14 04:04:45 +0700

    On Progress

---
+++ config/jwt.js
@@ -0,0 +1,7 @@
+// konfigurasi disimpan terpusat di file
+// supaya semua kode yang membutuhkan konfigurasi ini memiliki data yang seragam
+const JWT_SECRET = "secret";
+
+module.exports = {
+    JWT_SECRET,
+};
